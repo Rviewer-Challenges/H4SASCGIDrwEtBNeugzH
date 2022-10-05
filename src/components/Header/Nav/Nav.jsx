63334de0818360a7f8afa84f1096db41dc07952e
Gaizka79 <agaizka79@gmail.com>
2022-10-06 00:42:05 +0200

    convers ok

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -9,7 +9,7 @@ function Nav () {
       <Link to="/Exchange"><button type="button" className="bt_Nav">Exchange</button></Link>
       <Link to="/Peso"><button type="button" className="bt_Nav">Peso</button></Link>
       <Link to="/Velocidad"><button type="button" className="bt_Nav">Velocidad</button></Link>
-      <Link to="/Distancia"><button type="button" className="bt_Nav">Distancia</button></Link>
+      <Link to="/Distancia"><button type="button" className="bt_Nav">Longitud</button></Link>
       <Link to="/Volumen"><button type="button" className="bt_Nav">Volumen</button></Link>
     </nav>
   )
